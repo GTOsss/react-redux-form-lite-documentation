3f7dbf3c4ca9e56a8260d485870b4c617675b7eb
Timofey Goncharov <gtosss@mail.ru>
2020-01-08 20:54:16 +0300

    Change configuration, install addition modules.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,13 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,8 +25,15 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: 'gatsby-plugin-i18n',
+      options: {
+        langKeyDefault: 'en',
+        useLangKeyLayout: false
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
---
+++ src/data/messages/en.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'test': 'Test text for example !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+};
